f421047f36da22d660a86f97d1693a5dc3d34a7d
Mo Abdo <mo1989abo@gmail.com>
2024-06-16 22:37:12 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,18 +1,27 @@
-prijzen = {
-    "aardbei": 3,
-    "vanilla": 4,
-    "chocolade": 5,
-}
-print()
-for k, v in prijzen.items():
-    print(k, v)
-aanbieding = prijzen["aardbei"] * 0.8
-reclama_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €{aanbieding:2f}"
-reclama_tekst2 = reclama_tekst[:-4]
-reclama_tekst3 = reclama_tekst2.upper()
-reclama_tekst4 = reclama_tekst3.split()
-for el in reclama_tekst4:
-    if len(el) <= 5:
-        print(el.upper())
-    else:
-       print(el.lower())
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
+        "aardbei": 3,
+        "vanilla": 4,
+        "chocolade": 5,
+    }
+  
+    aanbieding = prijzen["aardbei"] * 0.8
+
+    reclama_tekst = f"Vandaag in de aanbieding: aardbei-ijs, 1 liter - slechts €{aanbieding:2f}"
+
+    reclama_tekst2 = reclama_tekst[:-4]
+
+    reclama_tekst3 = reclama_tekst2.upper()
+
+    reclama_tekst4 = reclama_tekst3.split()
+
+    for el in reclama_tekst4:
+        if len(el) <= 5:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()
